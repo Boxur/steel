d1d70b12bb8d5227ec9264489ffa9192a756a02b
Boxur <boxur@boxur.org>
2026-06-10 20:40:16 +0200

    Switched from using two seperate channels to one
    I really dont know why i didnt do that before

--- channel/src/lib.rs
+++ channel/src/lib.rs
@@ -1,32 +1,27 @@
 use std::sync::mpsc::{self, Receiver, SendError, Sender};
 
 #[derive(Debug)]
-pub struct ChannelA<T, U> {
+pub struct Channel<T, U> {
     sender: Sender<T>,
     receiver: Receiver<U>,
 }
-#[derive(Debug)]
-pub struct ChannelB<T, U> {
-    sender: Sender<U>,
-    receiver: Receiver<T>,
-}
 
-pub fn new<T, U>() -> (ChannelA<T, U>, ChannelB<T, U>) {
+pub fn new<T, U>() -> (Channel<T, U>, Channel<U, T>) {
     let (a_sender, b_receiver) = mpsc::channel();
     let (b_sender, a_receiver) = mpsc::channel();
     (
-        ChannelA {
+        Channel {
             sender: a_sender,
             receiver: a_receiver,
         },
-        ChannelB {
+        Channel {
             sender: b_sender,
             receiver: b_receiver,
         },
     )
 }
 
-impl<T, U> ChannelA<T, U> {
+impl<T, U> Channel<T, U> {
     pub fn send(&self, value: T) -> Result<(), SendError<T>> {
         self.sender.send(value)
     }
@@ -36,20 +31,6 @@ impl<T, U> ChannelA<T, U> {
     }
 
     pub fn try_recv(&self) -> Result<U, mpsc::TryRecvError> {
-        self.receiver.try_recv()
-    }
-}
-
-impl<T, U> ChannelB<T, U> {
-    pub fn send(&self, value: U) -> Result<(), SendError<U>> {
-        self.sender.send(value)
-    }
-
-    pub fn recv(&self) -> Result<T, mpsc::RecvError> {
-        self.receiver.recv()
-    }
-
-    pub fn try_recv(&self) -> Result<T, mpsc::TryRecvError> {
         self.receiver.try_recv()
     }
 }
